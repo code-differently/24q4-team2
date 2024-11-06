b2c2b73b503091be6990b1c8c1775ccbd06d8215
nilejack <njacksonnorthrup244@gmail.com>
2024-11-06 16:19:49 -0500

    Feat: Contingent logic add

--- Game/GameEngine.java
+++ Game/GameEngine.java
@@ -1,3 +1,5 @@
+import java.util.random;
+
 public class GameEngine {
   private boolean isStarted;
   private int numGenerator;
@@ -13,6 +15,13 @@ public class GameEngine {
         this.isStarted = false;
     }
   }
+  public int numGenerator(int numGenerator, int[]randomNum) {
+    int[] randomNum = List<int>Array[];//need  to figure out how to implement each number range for difficulty level*Easy/Medium/Hard Modes*
+    List<int>Array randomNum = new []
+    for(int i = 0; i < randomNum.length; i++ ); //This will more than likely be inapplicable for our implementation, just an idea.
+    int numSelector += randomNum.length[i] + Math.Random(); 
+
+  }
   // Getter for isStarted
   public boolean isStarted() {
     return isStarted; 
@@ -22,4 +31,5 @@ public class GameEngine {
   public int getNumGenerator() {
     return numGenerator;
   }
+
 }
