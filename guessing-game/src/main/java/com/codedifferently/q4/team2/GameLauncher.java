9cac63c747f537429e816b0c45b04dc082d5b0a5
jimoye244 <oyeyemijimoh244@gmail.com>
2024-11-06 15:57:18 -0500

    feat: added initial code base

---
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/GameLauncher.java
@@ -0,0 +1,16 @@
+package com.codedifferently.q4.team2;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@SpringBootApplication(scanBasePackages = "com.codedifferently.q4.team2")
+public class GameLauncher {
+    public static void main(String[] args) {
+        var application = new SpringApplication(GameLauncher.class);
+        application.run(args);
+    }
+}
