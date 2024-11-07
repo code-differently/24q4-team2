003e93bba642c83689a06bb373d4121bb8eb63cf
htanweer244 <htanweer244@gmail.com>
2024-11-07 18:46:22 -0500

    feat: Add: Second exception IllegalArgumentException

--- guessing-game/src/main/java/com/codedifferently/q4/team2/exception/IllegalArgumentException.java
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/exception/IllegalArgumentException.java
@@ -6,4 +6,9 @@ public class IllegalArgumentException extends RuntimeException {
 }
 }
 
+// Will throw IllegalArgumentException 
+//if the player selects a difficulty level that is not in the range of valid options (1, 2, or 3).
+
+// if (difficultyChoice < 1 || difficultyChoice > 3) {
+//  throw new IllegalArgumentException("Invalid difficulty level selected. Please select 1, 2, or 3."); }
 
