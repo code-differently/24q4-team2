f09d4498b9d0e52ac683aebc451d4f3caf11592a
htanweer244 <htanweer244@gmail.com>
2024-11-07 13:56:55 -0500

    feat: choosing the difficult levels for the game numMeCrazy (#13)
    * choosing the difficult levels for the game NumMeCrazy
    
    * feat: modified GameEngine file by adding difficulty levels
    
    * feat: modified GameEngine added difficulty levels
    
    * Delete guessing-game/src/main/java/com/codedifferently/q4/team2/NumMeCrazy.java

--- guessing-game/src/main/java/com/codedifferently/q4/team2/service/GameEngine.java
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/service/GameEngine.java
@@ -10,5 +10,21 @@ public class GameEngine {
     this.console = new Scanner(System.in);
   }
 
-  public void start() {}
+  public void start() {
+
+        System.out.println("Welcome to Guess Game - numMeCrazy");
+        System.out.println("Choose a difficulty level:");
+        System.out.println("1. Easy (1-10)");
+        System.out.println("2. Medium (1-20)");
+        System.out.println("3. Hard (1-50)");
+        System.out.print("Enter 1, 2, or 3 to select difficulty level, or 0 to exit: ");
+
+        int difficulty = console.nextInt();
+        
+        // Exit if user selects 0
+        if (difficulty == 0) {
+        System.out.println("Exiting the game.");
+            return;
+        }
+    }
 }
