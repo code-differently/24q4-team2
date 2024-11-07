58cf8b6cc057363af96fb381d0cdab1e5737a226
Oyeyemi Jimoh <oyeyemijimoh244@gmail.com>
2024-11-07 11:26:57 -0500

    feat: added packages and refactor some code (#9)

---
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/service/GameEngine.java
@@ -0,0 +1,14 @@
+package com.codedifferently.q4.team2.service;
+
+import java.util.Scanner;
+
+public class GameEngine {
+
+  private Scanner console;
+
+  public GameEngine() {
+    this.console = new Scanner(System.in);
+  }
+
+  public void start() {}
+}
