819a4ff06f02b135eb56c0076cde6812bc944dd4
htanweer244 <htanweer244@gmail.com>
2024-11-07 18:32:04 -0500

    feat: Adds Second Exception - IllegalArgumentException

---
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/exception/IllegalArgumentException.java
@@ -0,0 +1,9 @@
+package com.codedifferently.q4.team2.exception;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+}
+}
+
+
