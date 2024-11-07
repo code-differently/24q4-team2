32d376d88351f0b93bc95e7fbfa9856df49e8e73
jjcapparell <jjcapparell244@gmail.com>
2024-11-07 14:04:11 -0500

    chore: moved InvalidInputException.java into own exception folder

--- guessing-game/src/main/java/com/codedifferently/q4/team2/exception/InvalidInputException.java
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/exception/InvalidInputException.java
@@ -1,4 +1,4 @@
-package com.codedifferently.q4.team2;
+package com.codedifferently.q4.team2.exception;
 
 public class InvalidInputException extends RuntimeException {
     public InvalidInputException(String message) {
