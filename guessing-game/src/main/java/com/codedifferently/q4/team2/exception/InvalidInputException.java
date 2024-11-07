4a08d6512df033e63a4b217c7afce765d4f08d4b
nilejack <njacksonnorthrup244@gmail.com>
2024-11-07 11:19:37 -0500

    InvalidInputException class

---
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/exception/InvalidInputException.java
@@ -0,0 +1,8 @@
+package com.codedifferently.q4.team2.exception;
+
+public class InvalidInputException extends RuntimeException {
+        public InvalidInputException(String message) {
+            super(message);
+        }
+
+}
