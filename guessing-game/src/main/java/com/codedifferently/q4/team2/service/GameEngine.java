5e7eed1258ed53fe89f49a73b45cf5b99e5d95b6
jjcapparell <jjcapparell244@gmail.com>
2024-11-08 14:39:39 -0500

    chore: rearranged characters

--- guessing-game/src/main/java/com/codedifferently/q4/team2/service/GameEngine.java
+++ guessing-game/src/main/java/com/codedifferently/q4/team2/service/GameEngine.java
@@ -29,7 +29,8 @@ public class GameEngine {
     System.out.println("              Welcome to numMeCrazy!");
     System.out.println("**************                      **************");
     System.out.println("**************************************************\n");
-    System.out.println("Created by: Oyeyemi Jimoh, James Capparell, Hummad Tanweer, and Nile Jackson\n");
+    System.out.println(
+        "Created by: Oyeyemi Jimoh, James Capparell, Hummad Tanweer, and Nile Jackson\n");
     System.out.println("****** Please select Game difficulty level: ******\n");
     System.out.println("{1}. \t Easy (1-10)");
     System.out.println("{2}. \t Medium (1-20)");
@@ -57,13 +58,14 @@ public class GameEngine {
     }
     secretNumber = generateNumberToGuess();
     System.out.println("\n     You Selected " + level.name() + " difficulty level \n");
-    System.out.println("*************************************************\n\n\n");
+    System.out.println("*************************************************\n\n\n\n\n");
     System.out.print("Please enter your name :\n");
     console = new Scanner(System.in);
     playerName = console.nextLine();
     System.out.println("\n\n");
-    System.out.println("\n                 Hi, " + playerName + "!\n             Let's get Started!\n");
-    System.out.println("   Your Number GUESS must be Between 1 and " + level.value + "\n");
+    System.out.println(
+        "\n                Hi, " + playerName + "!\n            Let's get Started!\n");
+    System.out.println("  Your Number GUESS must be Between 1 and " + level.value + "\n");
     play();
   }
 
@@ -75,15 +77,22 @@ public class GameEngine {
       exitGame(guessedNumber);
       isCorrect = validateGuess(guessedNumber);
       String message = (secretNumber >= guessedNumber) ? "greater" : "less";
-      String result = (isCorrect) ? "<<<Correct>>>\n Congratulations!\n\nIt took ? attempts\n" : "Incorrect! \nThe expected number is " + message + " than " + guessedNumber+ "\n           Try Again!\n";
-      System.out.println("\n         "+ guessedNumber + " is " + result);
+      String result =
+          (isCorrect)
+              ? "<<<Correct>>>\n              Congratulations!\n\n             It took ? attempts\n"
+              : " Incorrect! \n    The expected number is "
+                  + message
+                  + " than "
+                  + guessedNumber
+                  + "\n                Try Again!\n";
+      System.out.println("\n\n\n                   " + guessedNumber + " is \n               " + result);
       if (guessedNumber == 0) {
         isGameOn = true;
       } else {
         isGameOn = false;
       }
       if (isCorrect) {
-        System.out.println("\nNew secret number generated ");
+        System.out.println("\n          New secret number generated \n");
         secretNumber = generateNumberToGuess();
       }
     }
@@ -102,12 +111,14 @@ public class GameEngine {
   void exitGame(int exitCode) {
     if (exitCode == 0) {
       System.out.println("\n\n\n\n");
-      System.out.println("Exiting the game ....\n");
-      System.out.println("Thank you for playing");
-      System.out.println("*********************");
-      System.out.println("*    numMeCrazy!    *");
-      System.out.println("*********************\n\n\n");
-      System.out.println("Created by: Oyeyemi Jimoh, James Capparell, Hummad Tanweer, and Nile Jackson\n\n");;
+      System.out.println("            Thank you for playing");
+      System.out.println("            *********************");
+      System.out.println("            *    numMeCrazy!    *");
+      System.out.println("            *********************\n");
+      System.out.println(
+          "                 Created by: \n             Oyeyemi Jimoh\n                 James Capparell\n             Hummad Tanweer\n                 Nile Jackson\n\n");
+      ;
+      System.out.println("Exiting the game ....\n\n");
       System.exit(exitCode);
     }
   }
