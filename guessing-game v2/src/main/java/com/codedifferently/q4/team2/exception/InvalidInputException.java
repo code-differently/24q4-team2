6e234b09f308caa301e59d9ff79117b7302d9f34
htanweer244 <htanweer244@gmail.com>
2024-12-04 16:45:11 -0500

    chore:made a copy of guessing-game

--- guessing-game v2/src/main/java/com/codedifferently/q4/team2/exception/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package com.codedifferently.q4.team2.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException(String message) {
-        super(message);
-    }
-
-}
-
