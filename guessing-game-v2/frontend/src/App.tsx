f32ba1c828b527fbac7f36c8e6bf6151c04f302f
jjcapparell <jjcapparell244@gmail.com>
2024-12-05 17:29:38 -0500

    feat: changes to App files and created Header and Play file

--- guessing-game-v2/frontend/src/App.tsx
+++ guessing-game-v2/frontend/src/App.tsx
@@ -1,19 +1,28 @@
 import Home from "./pages/Home";
+import Header from "./components/Header";
+import { useEffect, useState } from 'react';
 
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from './assets/numMeIcon.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Blog } from './components/Blog'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Fetch data from the Spring Boot API
+    fetch('http://localhost:8080/hello')
+      .then(response => response.text())
+      .then(data => setMessage(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
 
   return (
     <>
     <div>
+      <Header />
       <Home />
     </div>
     </>
