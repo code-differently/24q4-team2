f22d17c7e524aa0503d53d193d2cd5c39b399e21
htanweer244 <htanweer244@gmail.com>
2024-12-06 16:30:08 -0500

    chore: corrected the folder name

--- guessing-game-v2/src/main/java/com/codedifferently/q4/team2/repository/PlayerRespository.java
+++ guessing-game-v2/src/main/java/com/codedifferently/q4/team2/repository/PlayerRespository.java
@@ -1,6 +1,6 @@
 import com.codedifferently.q4.team2.model.Player;
 
-public interface PlayerRespository extends JpaRepository <Player,Integer> {
-    
+@Repository
+public interface PlayerRespository extends JpaRepository <Player,Integer> {    
 }
 
