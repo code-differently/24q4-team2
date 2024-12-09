af30c8606db29d5b9722077ab9ff5faad8fe193e
Nautevol07VII-111 <nilejack@udel.edu>
2024-12-09 10:42:40 -0500

    Feat Adds: prequisite for database communication(getters, setters for  play fields)

---
+++ guessing-game-v1/src/main/java/com/codedifferently/q4/team2/model/Play.java
@@ -0,0 +1,39 @@
+package com.codedifferently.q4.team2.model;
+
+public class Play {
+    private Long playTimeElapsed;
+    private int playAttempts;
+    private int playScore;
+
+    public Play(Long playTimeElapsed, int playAttempts, int playScore) {
+        this.playTimeElapsed = playTimeElapsed; 
+        this.playAttempts = playAttempts;
+        this.playScore = playScore;
+    }
+
+    public Long getPlayTimeElapsed() {
+        return playTimeElapsed;
+    }
+
+    public void setPlayTimeElapsed(Long playTimeElapsed) {
+        this.playTimeElapsed = playTimeElapsed;
+      }
+
+    public int getPlayAttempts() {
+        return playAttempts;
+    }
+    public void setPlayAttempts(int playAttempts) {
+        this.playAttempts = playAttempts;
+    }
+
+    public int getPlayScore() {
+        return playScore;
+    }
+
+    public void setPlayScore(int playScore) {
+        this.playScore = playScore;
+    }
+}
+
+
+
