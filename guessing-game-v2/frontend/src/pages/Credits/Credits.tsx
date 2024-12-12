c8a07ab93ec2398216301f7ce22c204daac0a46f
jjcapparell <jjcapparell244@gmail.com>
2024-12-12 15:12:56 -0500

    feat: added credits page with names of developers for the game

---
+++ guessing-game-v2/frontend/src/pages/Credits/Credits.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import credits from '../../assets/credits.png';
+
+export const Credits: React.FC = () => {
+    return (
+        <div className="background-2">
+            <div className="paper-background">
+            <img className="credit-names" src={credits} alt="credits" width="700" />
+                    <button className="title-button2">
+                        <a href="/">Title Screen</a>
+                    </button>
+                </div>
+            </div>
+    );
+    }
+
+export default Credits;
