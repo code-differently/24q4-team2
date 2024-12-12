74b4f55504de70dda8c79c5e7e799bff89917498
jjcapparell <jjcapparell244@gmail.com>
2024-12-12 15:01:21 -0500

    feat: added credits page with names of developers for the game

--- guessing-game-v2/frontend/src/main.tsx
+++ guessing-game-v2/frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import App from './App.tsx';
 import { Play } from './pages/Play/Play';
 import { Home } from './pages/Home/Home.tsx';
+import { Credits } from './pages/Credits/Credits.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
       {
         path: 'play',
         element: <Play />,
+      },
+      {
+        path: 'credits',
+        element: <Credits />
       },
     ],
   },
