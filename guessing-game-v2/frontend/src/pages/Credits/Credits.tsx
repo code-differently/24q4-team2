fb8464492caa19aeb6df2baffcbea2e013f01de8
James Capparell <jjcapparell244@gmail.com>
2024-12-12 15:56:52 -0500

    Feat: fixes for last credits pull (#52)
    * feat: added credits page with names of developers for the game
    
    * fix: credits screen fix
    
    * fix: credits screen fix
    
    ---------
    
    Co-authored-by: jjcapparell <“jjcapparell244@gmail.com>

--- guessing-game-v2/frontend/src/pages/Credits/Credits.tsx
+++ guessing-game-v2/frontend/src/pages/Credits/Credits.tsx
@@ -5,12 +5,14 @@ export const Credits: React.FC = () => {
     return (
         <div className="background-2">
             <div className="paper-background">
-            <img className="credit-names" src={credits} alt="credits" width="700" />
+                <div className="buttons">
+                    <img className="credit-names" src={credits} alt="credits" width="700" />
                     <button className="title-button2">
                         <a href="/">Title Screen</a>
                     </button>
                 </div>
             </div>
+        </div>
     );
     }
 
