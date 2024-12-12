fb1b4c5306b75f84ea52d7b478412af3e86adeef
jjcapparell <jjcapparell244@gmail.com>
2024-12-12 15:44:31 -0500

    fix: credits screen fix

--- guessing-game-v2/frontend/src/pages/Credits/Credits.tsx
+++ guessing-game-v2/frontend/src/pages/Credits/Credits.tsx
@@ -5,8 +5,8 @@ export const Credits: React.FC = () => {
     return (
         <div className="background-2">
             <div className="paper-background">
-                <img className="credit-names" src={credits} alt="credits" width="700" />
                 <div className="buttons">
+                    <img className="credit-names" src={credits} alt="credits" width="700" />
                     <button className="title-button2">
                         <a href="/">Title Screen</a>
                     </button>
